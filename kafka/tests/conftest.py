8763b73e3e160cd8998f13497749081f4452716b
Ashish Hanwadikar <ashish@kloudfuse.com>
2021-03-27 22:07:06 -0700

    Added option to kafka tests to have replicated cluster.

--- kafka/tests/conftest.py
+++ kafka/tests/conftest.py
@@ -8,15 +8,25 @@ import pytest
 from datadog_checks.dev import docker_run
 from datadog_checks.dev.conditions import CheckDockerLogs
 from datadog_checks.dev.utils import load_jmx_config
+from datadog_checks.base import is_affirmative
 
-from .common import HERE
-
+from .common import HERE, HOST, HOST_IP
 
 @pytest.fixture(scope='session')
 def dd_environment():
-    compose_file = os.path.join(HERE, 'compose', 'docker-compose.yml')
+    USE_MULTIPLE_BROKERS = is_affirmative(os.environ['USE_MULTIPLE_BROKERS'])
+    if USE_MULTIPLE_BROKERS:
+        compose_file = os.path.join(HERE, 'compose', 'multiple-brokers.yaml')
+    else:
+        compose_file = os.path.join(HERE, 'compose', 'single-broker.yaml')
 
     with docker_run(
-        compose_file, conditions=[CheckDockerLogs(compose_file, [r'\[KafkaServer id=\d+\] started'], matches="all")]
+        compose_file, conditions=[CheckDockerLogs(compose_file, [r'\[KafkaServer id=\d+\] started'], matches="all")],
+            env_vars={
+            # Advertising the hostname doesn't work on docker:dind so we manually
+            # resolve the IP address. This seems to also work outside docker:dind
+            # so we got that goin for us.
+            'KAFKA_HOST': HOST_IP
+        },
     ):
         yield load_jmx_config(), {'use_jmx': True}
